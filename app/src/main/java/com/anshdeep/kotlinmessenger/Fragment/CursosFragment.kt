27e5f06bb7ecc6f37c3733f9b34327028198bf56
Stivenmore <IngenieroStiven@outlook.es>
2020-05-12 23:19:54 -0500

    Fragment Completo con view

--- app/src/main/java/com/anshdeep/kotlinmessenger/Fragment/CursosFragment.kt
+++ app/src/main/java/com/anshdeep/kotlinmessenger/Fragment/CursosFragment.kt
@@ -1,39 +1,47 @@
 package com.anshdeep.kotlinmessenger.Fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-
 import com.anshdeep.kotlinmessenger.R
-import kotlinx.android.synthetic.main.fragment_cursos.*
+import de.hdodenhof.circleimageview.CircleImageView
+
 
 /**
  * A simple [Fragment] subclass.
  */
-@Suppress("UNREACHABLE_CODE")
 class CursosFragment : Fragment() {
-
+    private lateinit var fragment : Fragment
     private lateinit var adapter: MainAdapter
-    private lateinit var recyclerViewCurso: RecyclerView
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    private var recyclerViewCurs: RecyclerView? = null
 
-        return inflater.inflate(R.layout.fragment_cursos, container, false)
 
-        adapter = (this.context)?.let { MainAdapter(it) }!!
-        recyclerViewCurso.layoutManager = LinearLayoutManager(this.context)
-        recyclerViewCurso.adapter = adapter
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view: View = inflater.inflate(R.layout.fragment_cursos, container, false)
 
-        val dummyList = mutableListOf<Curso>()
-        dummyList.add(Curso("https://www.cursosgis.com/wp-content/uploads/2-25.jpg","CURSO PROFESIONAL DE PYTHON","https://codigofacilito.com/cursos/python-web",
-                "Curso Orientado al desarrollo Web"))
-
-        adapter.setListData(dummyList)
-        adapter.notifyDataSetChanged()
+        recyclerViewCurs = (view.findViewById<View>(R.id.recyclerViewCurso) as? RecyclerView)!!
+        fragment = this
+        return view
     }
+        override fun onActivityCreated(savedInstanceState: Bundle?) {
+            super.onActivityCreated(savedInstanceState)
+
+            this.adapter = (fragment).let { fragment.context?.let { it1 -> MainAdapter(it1) } }!!
+            recyclerViewCurs?.layoutManager = LinearLayoutManager(fragment.context)
+            recyclerViewCurs?.adapter = adapter
+
+            val dummyList = mutableListOf<Curso>()
+            dummyList.add(Curso("https://www.cursosgis.com/wp-content/uploads/2-25.jpg", "CURSO PROFESIONAL DE PYTHON", "https://codigofacilito.com/cursos/python-web",
+                    "Curso Orientado al desarrollo Web"))
+
+            adapter.setListData(dummyList)
+            adapter.notifyDataSetChanged()
+            return
+        }
 }
--- app/src/main/java/com/anshdeep/kotlinmessenger/Fragment/MainAdapter.kt
+++ app/src/main/java/com/anshdeep/kotlinmessenger/Fragment/MainAdapter.kt
@@ -8,35 +8,35 @@ import android.view.View
 import com.anshdeep.kotlinmessenger.R
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.curso_item_row.view.*
+import kotlinx.android.synthetic.main.fragment_cursos.view.*
 
 class MainAdapter(private val context: Context): RecyclerView.Adapter<MainAdapter.MainViewHolder>() {
 
 
     private var dataList = mutableListOf<Curso>()
-     fun setListData(data:MutableList<Curso>)
-     {
-         dataList = data
-     }
+    fun setListData(data: MutableList<Curso>) {
+        dataList = data
+    }
 
-    inner class MainViewHolder(itemView:View):RecyclerView.ViewHolder(itemView) {
-        fun bindView(curso:Curso){
+    inner class MainViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bindView(curso: Curso) {
             Glide.with(context).load(curso.imagenUrl).into(itemView.circleImageView)
-            itemView.txtTitle.text=curso.titulo
-            itemView.txtPage.text=curso.pagina
-            itemView.txtLink.text=curso.link
-            itemView.txtDesc.text=curso.descripcion
+            itemView.txtTitle.text = curso.titulo
+            itemView.txtPage.text = curso.pagina
+            itemView.txtLink.text = curso.link
+            itemView.txtDesc.text = curso.descripcion
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
-        val View = LayoutInflater.from(context).inflate(R.layout.curso_item_row,parent, false)
+        val View = LayoutInflater.from(context).inflate(R.layout.curso_item_row, parent, false)
         return MainViewHolder(View)
     }
 
     override fun getItemCount(): Int {
-        return if(dataList.size > 0){
+        return if (dataList.size > 0) {
             dataList.size
-        }else{
+        } else {
             0
         }
     }
